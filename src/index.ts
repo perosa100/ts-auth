fda9f5f773e0d46e41716e47a686a0cf57635844
perosa100 <patrickperosapp@gmail.com>
2020-07-02 00:05:04 -0300

    Finally

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata"
+import express from 'express'
+import './database/connect'
+import routes from './routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3001, () => console.log(`Servidor rodando porta http://localhost:3001`)) 
